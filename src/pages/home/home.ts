2391a4e3017211288ac4e440562b590b650a9dcb
emir <emir.ganimi@globant.com>
2019-07-23 15:45:08 -0400

    se termino de hacer el todo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public tasks: Array<Task> = [];
+  public task: string = '';
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  public addTask(){
+    if(this.task !== ''){
+      let newTask: Task   = new Task();
+      newTask.name        = this.task;
+      newTask.isChecked   = false;
+      this.tasks.push(newTask);
+      this.task = '';
+    }
+  }
+
+  public deleteTasks(){
+    let newArrayTask: Array<Task> = [];
+    for (let index = 0; index < this.tasks.length; index++) {
+      const element = this.tasks[index];
+      if(element.isChecked===false){
+        newArrayTask.push(element);
+      }
+    }
+    this.tasks = newArrayTask;
+  }
+}
+
+export class Task{
+  public name: string;
+  public isChecked: boolean;
 }
